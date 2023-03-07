431133a5c5599ce0184c680f5aa7131d44b475c5
kouheifujii <kohei.fujii@i-nations.tokyo>
2023-03-08 08:33:30 +0900

    データの挿入方法あれこれ

--- 9: db/main.go
+++ 9: db/main.go
@@ -5,7 +5,9 @@ package main
 import (
 	"context"
 	"database/sql"
+	"fmt"
 	"log"
+	"strings"
 	"time"
 
 	_ "github.com/jackc/pgx/v4/stdlib" // ブランクインポートして初期化のみ実施
@@ -32,20 +34,82 @@ func createTable(db *sql.DB) error {
 		log.Fatalf("create table error: %v", err)
 	}
 	// テーブルにデータを挿入する
-	insert := `
-		INSERT INTO users (user_id, user_name, created_at) VALUES ($1, $2, $3)
-	`
+	// insert := `
+	// 	INSERT INTO users (user_id, user_name, created_at) VALUES ($1, $2, $3)
+	// `
 	now := time.Now()
+	/*
+		パターン１:愚直に挿入
 
-	_, err = db.Exec(insert, "001", "testuser1", now)
-	if err != nil {
-		log.Fatalf("insert error: %v", err)
+		_, err = db.Exec(insert, "001", "testuser1", now)
+			if err != nil {
+				log.Fatalf("insert error: %v", err)
+			}
+
+			_, err = db.Exec(insert, "002", "testuser2", now)
+			if err != nil {
+				log.Fatalf("insert error: %v", err)
+			}
+	*/
+	/*
+		パターン２：プリペアードステートメントを使って挿入
+		プリペアードステートメントとは、SQLのテンプレートを作成しておき、あとは変数の値だけを指定して実行する。
+		プリペアードステートメントを使うことで、SQLのパースやコンパイルを1回だけ行うことができる。
+
+		users := []User{
+			{UserID: "001", UserName: "testuser1", CreatedAt: now},
+			{UserID: "002", UserName: "testuser2", CreatedAt: now},
+			{UserID: "003", UserName: "testuser3", CreatedAt: now},
+		}
+		ctx := context.Background()
+		stmt, err := db.PrepareContext(ctx, insert)
+		if err != nil {
+			log.Fatalf("prepare error: %v", err)
+			return err
+		}
+		defer stmt.Close() // プリペアードステートメントはClose()で閉じる必要がある
+
+		for _, user := range users {
+			// 構築したプリペアードステートメントにパラメータをセットして実行する
+			if _, err = stmt.ExecContext(ctx, user.UserID, user.UserName, user.CreatedAt); err != nil {
+				log.Fatalf("exec error: %v", err)
+				return err
+			}
+		}
+	*/
+	/*
+		パターン３：バッチインサートを使用する
+		バッチインサートとは、複数のINSERT文を1つのSQL文にまとめて実行することで、パフォーマンスを向上させる方法。
+		例：
+			INSERT INTO
+				users (user_id, user_name, created_at)
+			VALUES
+				('001', 'testuser1', '2021-01-01 00:00:00'),
+				('002', 'testuser2', '2021-01-01 00:00:00'),
+				('003', 'testuser3', '2021-01-01 00:00:00');
+	*/
+	users := []User{
+		{UserID: "001", UserName: "testuser1", CreatedAt: now},
+		{UserID: "002", UserName: "testuser2", CreatedAt: now},
+		{UserID: "003", UserName: "testuser3", CreatedAt: now},
+	}
+	// VALUESの部分を作成する
+	valueStrings := make([]string, 0, len(users))
+	valueArgs := make([]interface{}, 0, len(users)*3) // 3はカラム数
+	number := 1
+	for _, user := range users {
+		valueStrings = append(valueStrings, fmt.Sprintf("($%d, $%d, $%d)", number, number+1, number+2)) // $1, $2, $3
+		valueArgs = append(valueArgs, user.UserID)                                                      // $1にはuser.UserIDが入る
+		valueArgs = append(valueArgs, user.UserName)                                                    // $2にはuser.UserNameが入る
+		valueArgs = append(valueArgs, user.CreatedAt)                                                   // $3にはuser.CreatedAtが入る
+		number += 3                                                                                     // 3カラム文ずらす。$1, $2, $3の次は$4, $5, $6
+	}
+	query := fmt.Sprintf("INSERT INTO users (user_id, user_name, created_at) VALUES %s", strings.Join(valueStrings, ","))
+	if _, err = db.Exec(query, valueArgs...); err != nil { // valueArgs...でスライスの展開
+		log.Fatalf("exec error: %v", err)
+		return err
 	}
 
-	_, err = db.Exec(insert, "002", "testuser2", now)
-	if err != nil {
-		log.Fatalf("insert error: %v", err)
-	}
 	return nil
 }
 
